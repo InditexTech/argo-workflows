1136865c388b299a9ffb5105d5f058b8dba4275a
jonathancri <jonathancri@ext.inditex.com>
2024-03-22 16:08:18 +0100

    fix: fix comments in pr

--- server/auth/devhub/aplication.go
+++ server/auth/devhub/aplication.go
@@ -3,40 +3,17 @@ package devhub
 import (
 	"encoding/json"
 	"fmt"
-	"slices"
 )
 
-func GetDevhubServices(devhubclient *Client, apiUrl, apiPassword, userToIdentify string) ([]string, error) {
-	var servicesArray []string
-	apiDevhub := fmt.Sprintf("%s/api/identity/%s", apiUrl, userToIdentify)
-	res, err := HandleRequestApiInditex(devhubclient, apiDevhub, "GET", apiPassword, map[string]interface{}{})
-	if err != nil {
-		return nil, err
-	}
-	var result map[string]interface{}
-	if err := json.NewDecoder(res.Body).Decode(&result); err != nil {
-		return nil, err
-	}
-
-	if teams, ok := result["teams"].([]interface{}); ok {
-		for _, team := range teams {
-			if len(team.(map[string]interface{})["projects"].([]interface{})) > 0 {
-				for _, project := range team.(map[string]interface{})["projects"].([]interface{}) {
-					if relationshipType, ok := project.(map[string]interface{})["relationshipType"].(map[string]interface{}); ok && relationshipType["name"] == "Owner" {
-						ifInArray := slices.Contains(servicesArray, project.(map[string]interface{})["key"].(string))
-						if !ifInArray {
-							servicesArray = append(servicesArray, project.(map[string]interface{})["key"].(string))
-						}
-					}
-				}
-			}
-		}
-	}
-	return servicesArray, nil
+type RolesAndServices struct {
+	Roles    map[string]string
+	Services map[string]string
 }
 
-func GetDevhubRoles(devhubclient *Client, apiUrl, apiPassword, userToIdentify string) ([]string, error) {
-	var rolesArray []string
+func GetServicesAndRoles(devhubclient *Client, apiUrl, apiPassword, userToIdentify string) (*RolesAndServices, error) {
+	roles := make(map[string]string)
+	services := make(map[string]string)
+	servicesAndRoles := &RolesAndServices{}
 	apiDevhub := fmt.Sprintf("%s/api/identity/%s", apiUrl, userToIdentify)
 	res, err := HandleRequestApiInditex(devhubclient, apiDevhub, "GET", apiPassword, map[string]interface{}{})
 	if err != nil {
@@ -51,17 +28,16 @@ func GetDevhubRoles(devhubclient *Client, apiUrl, apiPassword, userToIdentify st
 			if len(team.(map[string]interface{})["projects"].([]interface{})) > 0 {
 				for _, project := range team.(map[string]interface{})["projects"].([]interface{}) {
 					if relationshipType, ok := project.(map[string]interface{})["relationshipType"].(map[string]interface{}); ok && relationshipType["name"] == "Owner" {
+						services[project.(map[string]interface{})["key"].(string)] = "service"
 						for _, profile := range team.(map[string]interface{})["profiles"].([]interface{}) {
-							ifInArray := slices.Contains(rolesArray, profile.(map[string]interface{})["name"].(string))
-							if !ifInArray {
-								rolesArray = append(rolesArray, profile.(map[string]interface{})["name"].(string))
-							}
+							roles[profile.(map[string]interface{})["name"].(string)] = "role"
 						}
 					}
 				}
 			}
-
 		}
 	}
-	return rolesArray, nil
+	servicesAndRoles.Roles = roles
+	servicesAndRoles.Services = services
+	return servicesAndRoles, nil
 }
