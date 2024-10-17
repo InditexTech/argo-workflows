c5d2daa24b1ab0d36ec68a1036abf038beb08214
jonathancri <jonathancri@ext.inditex.com>
2024-10-17 09:16:16 +0200

    feat: add support to new crossprofiles

--- config/sso_extended.go
+++ config/sso_extended.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"slices"
 	"strings"
 
 	"github.com/argoproj/argo-workflows/v3/server/auth/devhub"
@@ -33,26 +32,16 @@ func RbacDelegateToLabel(ctx context.Context, mail string, apiUrl, apiPassword, 
 	resourcesToFilterPopulated := &ResourcesToFilter{}
 	devhubClient := devhub.NewClient()
 	mailParts := strings.Split(mail, "@")
-	servicesAndRoles, err := devhub.GetServicesAndRoles(devhubClient, apiUrl, apiPassword, mailParts[0])
+	servicesAndGroup, err := devhub.GetServicesAndGroup(devhubClient, apiUrl, apiPassword, mailParts[0], writeGroups)
 	if err != nil {
 		fmt.Printf("Can't Procces the petition on devhub to get roles %+v", err)
 	}
-	resourcesToFilterPopulated.Group = getUserGroup(servicesAndRoles.Roles, writeGroups)
-	if servicesAndRoles.Services != nil {
-		for service := range servicesAndRoles.Services {
+	resourcesToFilterPopulated.Group = servicesAndGroup.Group
+	if servicesAndGroup.Services != nil {
+		for service := range servicesAndGroup.Services {
 			resourcesToFilterPopulated.ArrayLabels = append(resourcesToFilterPopulated.ArrayLabels, service)
 		}
 		resourcesToFilterPopulated.LabelsFilter = fmt.Sprintf("%s in (%s)", label, strings.Join(resourcesToFilterPopulated.ArrayLabels[:], ","))
 	}
 	return resourcesToFilterPopulated, nil
-}
-
-func getUserGroup(roles map[string]string, writeGroups []string) string {
-	sabyRole := "reader"
-	for role := range roles {
-		if slices.Contains(writeGroups, role) {
-			sabyRole = "writer"
-		}
-	}
-	return sabyRole
 }
--- server/auth/devhub/aplication.go
+++ server/auth/devhub/aplication.go
@@ -3,41 +3,63 @@ package devhub
 import (
 	"encoding/json"
 	"fmt"
+	"slices"
 )
 
-type RolesAndServices struct {
-	Roles    map[string]string
+type GroupAndServices struct {
 	Services map[string]string
+	Group    string
 }
 
-func GetServicesAndRoles(devhubclient *Client, apiUrl, apiPassword, userToIdentify string) (*RolesAndServices, error) {
+func GetServicesAndGroup(devhubclient *Client, apiUrl, apiPassword, userToIdentify string, writeGroups []string) (*GroupAndServices, error) {
+	var result map[string]interface{}
 	roles := make(map[string]string)
 	services := make(map[string]string)
-	servicesAndRoles := &RolesAndServices{}
+	servicesAndGroup := &GroupAndServices{}
 	apiDevhub := fmt.Sprintf("%s/api/identity/%s", apiUrl, userToIdentify)
 	res, err := HandleRequestApiInditex(devhubclient, apiDevhub, "GET", apiPassword, map[string]interface{}{})
 	if err != nil {
 		return nil, err
 	}
-	var result map[string]interface{}
 	if err := json.NewDecoder(res.Body).Decode(&result); err != nil {
 		return nil, err
 	}
 	if teams, ok := result["teams"].([]interface{}); ok {
-		for _, team := range teams {
-			if len(team.(map[string]interface{})["projects"].([]interface{})) > 0 {
-				for _, project := range team.(map[string]interface{})["projects"].([]interface{}) {
-					if relationshipType, ok := project.(map[string]interface{})["relationshipType"].(map[string]interface{}); ok && relationshipType["name"] == "Owner" {
-						services[project.(map[string]interface{})["key"].(string)] = "service"
-						for _, profile := range team.(map[string]interface{})["profiles"].([]interface{}) {
-							roles[profile.(map[string]interface{})["name"].(string)] = "role"
+		roles, services = GetRolesAndServices(teams, services, roles)
+	}
+
+	servicesAndGroup.Group = GetGroupByRole(writeGroups, roles)
+	servicesAndGroup.Services = services
+	return servicesAndGroup, nil
+}
+
+func GetRolesAndServices(teams []interface{}, services, roles map[string]string) (map[string]string, map[string]string) {
+	for _, team := range teams {
+		if len(team.(map[string]interface{})["projects"].([]interface{})) > 0 {
+			for _, project := range team.(map[string]interface{})["projects"].([]interface{}) {
+				if relationshipType, ok := project.(map[string]interface{})["relationshipType"].(map[string]interface{}); ok && relationshipType["name"] == "Owner" {
+					services[project.(map[string]interface{})["key"].(string)] = "service"
+					for _, profile := range team.(map[string]interface{})["profiles"].([]interface{}) {
+						roles[profile.(map[string]interface{})["name"].(string)] = "role"
+					}
+					if len(team.(map[string]interface{})["effectiveCrossProfiles"].([]string)) > 0 {
+						for _, effectiveCrossProfile := range team.(map[string]interface{})["effectiveCrossProfiles"].([]string) {
+							roles[effectiveCrossProfile] = "role"
 						}
 					}
 				}
 			}
 		}
 	}
-	servicesAndRoles.Roles = roles
-	servicesAndRoles.Services = services
-	return servicesAndRoles, nil
+	return roles, services
+}
+
+func GetGroupByRole(writeGroups []string, roles map[string]string) string {
+	groupByRole := "reader"
+	for role := range roles {
+		if slices.Contains(writeGroups, role) {
+			groupByRole = "writer"
+		}
+	}
+	return groupByRole
 }
